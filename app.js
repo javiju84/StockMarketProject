26638c44212c22013ce124bc825f8e8dd3ba693c
Javier <javiju84@gmail.com>
2016-04-24 18:33:03 +0200

    lectura url json ibex35, obtenes los datos (date,open,high,low,close,vol,adj.vol) de una sesion indicandolo en la array

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 var express = require ("express");
 var app = express();
+var request = require("request");
+
+var url = "https://www.quandl.com/api/v3/datasets/YAHOO/INDEX_IBEX.json"
+
+request({
+	url: url,
+	json:true
+}, function (error, response, body){
+
+	if (!error && response.statusCode === 200) {
+		console.log(body.dataset.data[200]);
+	}
+});
+
+
+
 app.listen(8080);
 console.log("Servidor conectado  puerto 8080")
