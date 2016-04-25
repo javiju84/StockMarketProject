f2f46092f41fc2daddcdceccf7c5aa1cccba8b21
Javier <javiju84@gmail.com>
2016-04-25 18:10:53 +0200

    ejemplo database url con mongoskin

--- app.js
+++ app.js
@@ -1,38 +1,56 @@
 var express = require ("express");
+var http = require ("http");
 var app = express();
-var Index_Ibex35 = require("./models/index_ibex35").Index_Ibex35;//llamamos al Schema,librerias
+var mongo = require ("mongoskin");
+var db = mongo.db("mongodb://localhost/zakazka", {native_parser : true});
+
 var request = require("request");
 
-var url = "https://www.quandl.com/api/v3/datasets/YAHOO/INDEX_IBEX.json"
 
-request({
-	url: url,
-	json:true
-}, function (error, response, body){
 
-	if (!error && response.statusCode === 200) {
-			//console.log(body.dataset.data); /*obtener todos los datos de date*/
-			//console.log(body.dataset.dataset_code);/*obtener el nombre de la tabla 'INDEX_IBEX'*/
-			console.log(body.dataset.data);
-		var parseo = body.dataset.data;
+var site = "http://www.vsechnyzakazky.cz/api/v1/zakazka/?format=json&limit=2";
 
-	var jsonString=[];
-		for (var i=0; i < parseo.leght; i++){
-			var jsonDato = {};
-			jsonDato.fecha = parseo[i][0];
-			jsonDato.apertura = parseo[i][1];
-			jsonDato.alto = parseo[i][2];
-			jsonDato.bajo = parseo[i][3];
-			jsonDato.cierre = parseo[i][4];
-			jsonDato.volumen = parseo[i][5];
-			jsonDatp.ajuste_cierre = parseo[i][6];
-			jsonString.push(jsonDato);
-		} 
-		var jsonArray = JSON.parse(JSON.stringify(jsonString));
-		console.log(jsonArray);*/
-	}
-});
+function getData(cb){
+	http.get(site,function(res){
+		res.setEncoding("utf-8");
 
+		var body = " ";
+		res.on("data",function(d){
+			body += d;
+		});
+
+		res.on("end", function(){
+			try {
+				var parsed = JSON.parse(body);
+			} catch (err) {
+				console.error("Unable to parse response as JSON", err);
+				return cb(err);
+			}
+
+			cb(
+				parsed.objects
+				);
+		});
+	}).on("error", function(err){
+		console.error("Error with the request:", err.message);
+		cb(err);
+	});
+}
+
+function writeData (data, allGood){
+	console.log("writing");
+	console.log(typeof data);
+	console.log(data);
+
+	db.collection("zakazky").insert(data, function(error,record){
+		if (error) throw error;
+		console.log("data saved")
+	});
+}
+
+function allGood(){console.log("all done");}
+
+getData(writeData);
 
 
 app.listen(8080);
